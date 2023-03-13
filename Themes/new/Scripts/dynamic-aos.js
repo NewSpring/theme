46848ea1420ac204ee3b1dedc533b245c15fb76b
Brian Kalwat <brian@briankalwat.com>
2023-03-13 16:14:32 -0400

    updates to easter page and styles

--- Themes/new/Scripts/dynamic-aos.js
+++ Themes/new/Scripts/dynamic-aos.js
@@ -1,8 +1,9 @@
 document.addEventListener(
   "DOMContentLoaded",
   function () {
+    let content = document.querySelector("#content");
     // Get elements to animate
-    let elementsToAnimate = document.querySelectorAll(
+    let elementsToAnimate = content.querySelectorAll(
       "h1:not([data-aos], [data-aos] *), h2:not([data-aos], [data-aos] *), h3:not([data-aos], [data-aos] *), h4:not([data-aos], [data-aos] *), h5:not([data-aos], [data-aos] *), h6:not([data-aos], [data-aos] *), p:not([data-aos], [data-aos] *), i:not([data-aos], [data-aos] *), .dropdown-toggle"
     );
 
@@ -26,7 +27,7 @@ document.addEventListener(
       // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
       offset: 100, // offset (in px) from the original trigger point
       delay: 0, // values from 0 to 3000, with step 50ms
-      duration: 400, // values from 0 to 3000, with step 50ms
+      duration: 500, // values from 0 to 3000, with step 50ms
       easing: "ease-in-out", // default easing for AOS animations
       once: false, // whether animation should happen only once - while scrolling down
       mirror: false, // whether elements should animate out while scrolling past them
