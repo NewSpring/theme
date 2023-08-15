166f1e7a612d9339f09d15e546c1dd52ff0cf4ad
Brian Kalwat <brian@briankalwat.com>
2023-08-15 15:02:05 -0400

    updated overflow, slight modifications to dynamic aos script

--- Themes/new/Scripts/dynamic-aos.js
+++ Themes/new/Scripts/dynamic-aos.js
@@ -30,7 +30,7 @@ document.addEventListener(
       throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
       // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-      offset: 50, // offset (in px) from the original trigger point
+      offset: 100, // offset (in px) from the original trigger point
       delay: 0, // values from 0 to 3000, with step 50ms
       duration: 500, // values from 0 to 3000, with step 50ms
       easing: "ease-in-out", // default easing for AOS animations
