6969d50620e4cefd812538ee6b398cf241f03650
Brian Kalwat <brian@briankalwat.com>
2023-08-16 12:00:29 -0400

    finished up routing

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+const publicDirPath = path.join(__dirname, "./public");
+
+app.use(express.static(publicDirPath, { extensions: ["html"] }));
+app.use(express.static(__dirname));
+
+app.use(function (req, res) {
+  res.status(404).sendFile(publicDirPath + "/404.html");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log("Error", err);
+  }
+  console.log("Server started at http://localhost:" + port);
+});
