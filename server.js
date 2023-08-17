70e4b5c67215d83c70764109d2dfd03d3683df7c
Brian Kalwat <brian@briankalwat.com>
2023-08-17 09:02:38 -0400

    updated port to 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, "./public");
 
 app.use(express.static(publicDirPath, { extensions: ["html"] }));
